2b1bacdb8eb8834bef2fcf0cdbb4b8ff193388cb
fazouane-marouane <fazouanem3@gmail.com>
2016-12-05 01:43:24 +0100

    getting nasm

--- build.ps1
+++ build.ps1
@@ -3,6 +3,7 @@ try {
 Import-Module ./helpers -Global
 
 # I. Clone Nginx
+Remove-Item -Path (Get-Local "nginx") -Force -Recurse -ErrorAction SilentlyContinue
 $local:repo = Clone-Repository -RepoUri:"https://github.com/nginx/nginx.git" -Destination:(Get-Location) |
     Checkout-Tag -TagName:"release-1.10.2" |
     Apply-Patch -PatchLocation:(Resolve-Path "nginx.patch")
@@ -16,7 +17,8 @@ try{
 $local:dependencies=@(
     [Uri]"https://github.com/openssl/openssl/archive/OpenSSL_1_0_2j.zip",
     [Uri]"ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.39.zip",
-    [Uri]"http://zlib.net/zlib128.zip"
+    [Uri]"http://zlib.net/zlib128.zip",
+    [Uri]"http://www.nasm.us/pub/nasm/releasebuilds/2.12.02/win32/nasm-2.12.02-win32.zip"
 ) | ForEach-Object { [PSCustomObject]@{ Uri=$_; File=(Get-Local -Name ($_.Segments[-1]))} }
 # Download dependencies & Unzip them
 $local:dependencies | ForEach-Object { (New-Object Net.WebClient).DownloadFile($_.Uri, $_.File) }
@@ -24,6 +26,12 @@ $local:dependencies | ForEach-Object { Expand-Archive -Path:$_.File -Destination
 Rename-Item -Path:(Get-Local -Name:"openssl-OpenSSL_1_0_2j") "openssl"
 Rename-Item -Path:(Get-Local -Name:"pcre-8.39") "pcre"
 Rename-Item -Path:(Get-Local -Name:"zlib-1.2.8") "zlib"
+Rename-Item -Path:(Get-Local -Name:"nasm-2.12.02") "nasm"
+foreach($item in @( (Get-Local "nasm"), (Combine-Paths -PathParts @("nasm", "rdoff")) )){
+    if(-Not ($env:Path.Split(";") -contains $item)){
+        $env:Path = "$item;$env:Path"
+    }
+}
 }
 finally {
 Pop-Location
